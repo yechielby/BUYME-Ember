7d9f06ff833d7f038dfa83451ee2e991a43f5432
yechielby <yechielby@gmail.com>
2021-01-12 08:05:08 +0200

    login controller

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,4 +1,18 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+  email: null,
+  password: null,
+  session: Ember.inject.service(),
+  actions: {
+    authenticate() {
+      this.get('session').authenticate(this.get('email'),this.get('password'))
+      .then( ()=> {
+        alert('Logged in!');
+        this.transitionToRoute('tasks');
+      }, (err)=> {
+        alert(`Error with login! ${err.responseText}`);
+      });
+    }
+  }
 });
