84a348980ab330157001ada4d9c73b6d7b7e51e7
yechielby <yechielby@gmail.com>
2021-01-12 07:43:15 +0200

    send Authorization use Adapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,4 +1,12 @@
 import DS from 'ember-data';
+import EmberResolver from 'ember-resolver';
 
-export default DS.JSONAPIAdapter.extend({
+export default DS.RESTAdapter.extend({
+  namespace: 'api',
+  session: Ember.inject.service(),
+  headers: Ember.computed('session.token', function() {
+    return {
+      'Authorization': `Bearer ${this.get('session.token')}`
+    }
+  })
 });
