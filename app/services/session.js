5be09a81ee59d03f968c93262340776c65fe2f13
yechielby <yechielby@gmail.com>
2021-01-12 07:29:41 +0200

    use with token by service

--- app/services/session.js
+++ app/services/session.js
@@ -1,4 +1,14 @@
 import Service from '@ember/service';
 
 export default Service.extend({
+  token: null,
+  authenticate(email, pass) {
+    return Ember.$.ajax({
+      method: 'POST',
+      url: 'http://localhost:8000/api/user-login',
+      data: {email: email, password: pass}
+    }).then((info)=>{
+      this.set('token',info.success.token);
+    })
+  }
 });
