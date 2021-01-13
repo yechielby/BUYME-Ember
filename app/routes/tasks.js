d8ff468ca58f4c2a478b29751cb59925dcf2aa72
yechielby <yechielby@gmail.com>
2021-01-13 18:19:17 +0200

    if is not login go go home page

--- app/routes/tasks.js
+++ app/routes/tasks.js
@@ -1,6 +1,13 @@
 import Route from '@ember/routing/route';
+import { inject } from '@ember/service';
 
 export default Route.extend({
+  session: inject(),
+  beforeModel() {
+    if(!this.get('session.token')) {
+        this.transitionTo('/');
+    }
+  },
   model() {
     return this.store.findAll('task');
   }
